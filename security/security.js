4b181e873c2a8764d0e4ff160bdac20b28c1667c
Terminal-Est <cstephen1982@gmail.com>
2023-02-22 23:23:59 +1100

    Add User route built. Promise architecture built.

--- security/security.js
+++ security/security.js
@@ -2,6 +2,20 @@ var bcrypt = require('bcrypt');
 var dbapi = require('../dbapi/dbapi');
 
 function hashPassword(uid, password) {
+    
     const saltRounds = 10;
-     
+    this.password = password;
+    this.uid = uid;
+    const outer = this;
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) throw err;
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) throw err;    
+            dbapi.setPassHash(outer.uid, hash).catch(error => {
+                throw (error);
+            });
+        });  
+    });
 }
+
+module.exports = { hashPassword };

5c02e87e1f46918b85ab8967dbe23f900e91035f
Terminal-Est <cstephen1982@gmail.com>
2024-03-12 22:34:38 +1100

    Remove migrations, TypeORM doesn't like migrations based on multiple data sources.

--- data/data-source.ts
+++ data/data-source.ts
@@ -18,6 +18,25 @@ export const UserDataSource = new DataSource({
     migrations: ['dist/migrations/*.js'],
 })
 
+export const AppDataSource = new DataSource({
+    type: "mssql",
+    host: "progprojdb.database.windows.net",
+    port: 1433,
+    username: "app_data_login",
+    password: "Distinct_Iguana_670",
+    database: "ProgProjAppDb",
+    logging: ["error", "query", "schema"],
+    migrations: ['dist/migrations/*.js']
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        logging.logToFile("App data source initilized!");
+    })
+    .catch((err) => {
+        logging.logToFile("Error initilizing app data source : " + err);
+    })
+
 UserDataSource.initialize()
     .then(() => {
         logging.logToFile("User data source initialized!");

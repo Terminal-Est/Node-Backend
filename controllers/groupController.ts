dedcf1cc3f0312dc979e6495c0af8c4c034da7f1
Mark Sasse <sasse.mark@mazenod.wa.edu.au>
2024-03-29 18:09:10 +0800

    Initial functions to get data from the database.

--- controllers/groupController.ts
+++ controllers/groupController.ts
@@ -1,7 +1,19 @@
+import { AppDataSource, UserDataSource } from "../data/data-source";
+import { Group } from "../data/entity/group";
+import { UserGroup } from "../data/entity/userGroup";
+
 async function getGroups() {
-    
+    return await AppDataSource.getRepository(Group)
+    .createQueryBuilder("group")
+    .getMany();
 }
 
-async function joinGroup() {
-    
+async function joinGroup() {uuid: number, groupid: number) {
+    return await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(Group)
+        .values([
+            { uuid: Number, groupid: Number }
+        ])
+        .execute();
 }

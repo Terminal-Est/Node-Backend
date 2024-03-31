6fa91cb9db86b3b2f726397b9166e3d2c84d3141
Christopher Stephen <s3172455@student.rmit.edu.au>
2024-04-01 00:06:17 +1100

    Building out follow functionality.

--- controllers/followController.ts
+++ controllers/followController.ts
@@ -1,2 +1,15 @@
 import { AppDataSource } from "../data/data-source";
+import { UserFollows } from "../data/entity/userFollows";
 import { validate } from "class-validator";
+
+async function validateUserFollows(userFollows: UserFollows) {
+    const errors = await validate(UserFollows)
+    return new Promise<boolean>(function(resolve, reject) {
+        if (errors.length > 0) {
+            return reject(errors);
+        } else {
+            return resolve(true)
+        }
+    });
+}
+

46b893bc5b7ec993db6b184f161a995c6f5de112
Terminal-Est <cstephen1982@gmail.com>
2024-05-12 16:05:55 +1000

    Removed tests folder.

--- __tests__/azureContainer.test.ts
+++
@@ -1,8 +0,0 @@
-import { createBlobStorageContainer } from '../controllers/fileController';
-
-test('Create azure file storage container', () => {
-    createBlobStorageContainer('test').then((fulfilled: any) => {
-        console.log(fulfilled);
-        expect(typeof fulfilled).toBe('string');
-    })
-});

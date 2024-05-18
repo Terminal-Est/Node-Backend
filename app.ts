3f24fe382ab2e9ed70b12fd34c8e4ecf4d0b1641
Christopher Stephen <s3172455@student.rmit.edu.au>
2024-05-18 16:32:49 +1000

    Don't code tired folks.

--- app.ts
+++ app.ts
@@ -200,7 +200,7 @@ app.post('/login', fieldsOnly, loginRouter);
 
 // Post route for video upload
 var addVideoRouter = require('./routes/addVideo');
-app.post('/video', uploads.single('video'), /**adminValidationRouter, jwtHandler.validateJWT,**/ addVideoRouter);
+app.post('/video', uploads.single('video'), adminValidationRouter, jwtHandler.validateJWT, addVideoRouter);
 
 // Delete video from storage.
 var deleteVideoRouter = require('./routes/deleteVideo');
